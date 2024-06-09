907d1c7c414b20db8b3f22483f5b2d3d9ec88115
Esther Isimekhai <isimekhai@student.tugraz.at>
2024-06-09 14:44:04 +0200

    Finished Task 1

--- assign3_task1.py
+++ assign3_task1.py
@@ -11,75 +11,91 @@ STEREO_SCALE_FACTOR = 5.4
 if __name__ == '__main__':
 
     image_dir = os.path.join("data_ass3", "Task1_3", "images")
-    image_file = "2011_10_03_drive_0047_sync_image_0000000005_image_03.png"
 
     placeholder = ["assign3_task1.py"]
-    image_path = ["--image_path", os.path.join(image_dir, image_file)]
-    #image_path = ["--image_path", image_dir]
-    model_name = ["--model_name", "mono+stereo_640x192"]
+
+    # Comment out when predicting single files
+    image_path = ["--image_path", image_dir]
     search_for = ["--ext", "png"]
+    #
+    # Use th following for single file prediction
+    #image_file = "2011_10_03_drive_0047_sync_image_0000000005_image_03.png"
+    #image_path = ["--image_path", os.path.join(image_dir, image_file)]
+    # 
+
+    model_name = ["--model_name", "mono+stereo_640x192"]
+    #predict_depth = ["--pred_metric_depth"]
 
     # -------------------------------------------------------------------------
 
-    sys.argv = placeholder + image_path + model_name #+ search_for
+    sys.argv = placeholder + image_path + model_name  + search_for #+ predict_depth
     args = parse_args() 
     test_simple(args)
 
     # -------------------------------------------------------------------------
 
-    gt_image_dir = os.path.join("data_ass3", "Task1_3", "groundtruth")
-    gt_image_file = "2011_10_03_drive_0047_sync_groundtruth_depth_0000000005_image_03.png"
-
-    gt_image = cv2.imread(os.path.join(gt_image_dir, gt_image_file))
-
-    if gt_image is None:
-        print("Error: Could not load image from")
-        print(os.path.join(gt_image_dir, image_file))
-        exit()
-
-    file_name, file_extension = os.path.splitext(image_file)
-    file_name_wo_extension = os.path.basename(file_name)
-    depth_output_fname = os.path.join(image_dir, "{}_disp.jpeg".format(file_name_wo_extension)) 
-
-    depth_image = cv2.imread(depth_output_fname)
-
-    if depth_image is None:
-        print("Error: Could not load image from")
-        print(depth_output_fname)
-        exit()
-
+    image_dir_files = os.listdir(image_dir)
+        
+        # Filter out directories and non-png files, keep only .png files
+    image_paths = [f for f in image_dir_files 
+                    if os.path.isfile(os.path.join(image_dir, f))
+                    and f.lower().endswith('.png')]
+    
     # -------------------------------------------------------------------------
 
-    gt_image = gt_image.astype(float)
+    for i in range(len(image_paths)): 
 
-    mask = gt_image > 0
-    gt_image[mask] = 1 / gt_image[mask]
+        image_pattern = f'_sync_image_'
+        groundtruth_pattern = f'_sync_groundtruth_depth_'
 
-    print(gt_image.shape)
-    #print(gt_image)
-    print(np.max(gt_image))
-    print(np.min(gt_image[mask]))
+        image_path = image_paths[i]
+        file_name, file_extension = os.path.splitext(image_path)
 
-    # -------------------------------------------------------------------------
+        gt_image_dir = os.path.join("data_ass3", "Task1_3", "groundtruth")
+        gt_image_file = image_path.replace(image_pattern, groundtruth_pattern)
 
-    depth_image = depth_image.astype(float)
-    mask = depth_image > 0
-    depth_image[mask] = 1 / (depth_image[mask] * STEREO_SCALE_FACTOR)
+        gt_image = cv2.imread(os.path.join(gt_image_dir, gt_image_file), cv2.IMREAD_UNCHANGED)
 
-    print(depth_image.shape)
-    #print(depth_image)
-    print(np.max(depth_image))
-    print(np.min(depth_image[mask]))
+        if gt_image is None:
+            print("Error: Could not load image from")
+            print(os.path.join(gt_image_dir, gt_image_file))
+            exit()
 
-    # -------------------------------------------------------------------------
+        depth_npy_fname = os.path.join(image_dir, "{}_disp.npy".format(file_name)) 
+        depth_pred = np.load(depth_npy_fname)
 
-    errors = np.abs(gt_image - depth_image)
-    mask = errors > 0
-    errors[mask] = (80 * errors[mask])
+        if not depth_pred.any():
+            print("Error: Could not load image from")
+            print(depth_npy_fname) 
+            exit()
 
-    errors = np.round(errors)
-    print(errors)
-    # print(result_image.shape)
-    # cv2.imshow(result_image)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+        # Postprocessing
+        scaled_gt = ((gt_image / 256))
+
+        gt_height, gt_width = gt_image.shape[:2]
+
+        scaled_depth_pred = STEREO_SCALE_FACTOR * np.squeeze(depth_pred)
+        scaled_depth_pred = (cv2.resize(scaled_depth_pred, (gt_width, gt_height)))
+        # 
+
+        # Debug
+        # print(np.max(scaled_gt))    
+        # print(np.min(scaled_gt))    
+        # print(np.max(depth_pred))    
+        # print(np.min(depth_pred))    
+
+        # print(np.sum(diff))
+        # cv2.imshow("Prediction", scaled_depth_pred)
+        # cv2.waitKey(0)
+        
+        # cv2.imshow("Ground Truth", scaled_gt)
+        # cv2.waitKey(0)
+
+        # cv2.imshow("Difference", np.abs(scaled_depth_pred - scaled_gt))
+        # cv2.waitKey(0)
+        #
+
+        # rmse = (scaled_gt - scaled_depth_pred) ** 2
+        # rmse = np.sqrt(rmse.mean())
+
+        # print(rmse) 
